584d4b5f8fc2a298e5fb1c9f92116d2d133ccc43
Jia Li <101173025+Michael-Li-2002@users.noreply.github.com>
2024-01-26 14:19:59 +0800

    Update EM_1.m

--- Project02/code/EM_1.m
+++ Project02/code/EM_1.m
@@ -1,15 +1,15 @@
 function U = EM_1(pde,point,tau)
-%EM_1: Euler-Maruyama, 及简单的确定 Exit Time 的方式
+%EM_1: Euler-Maruyama, 录掳录貌碌楼碌脛脠路露篓 Exit Time 碌脛路陆脢陆
 x0 = point; 
 x1 = x0 + pde.b(x0) * tau + randn(1,2) * sqrt(tau);
 intf = 0;
 while pde.inDomain(x1)
-    if rand() >= exp(-2*(1-sqrt(sum(x0.^2)))*(1-sqrt(sum(x1.^2)))/tau)
+    % if rand() >= exp(-2*(1-sqrt(sum(x0.^2)))*(1-sqrt(sum(x1.^2)))/tau)
         intf = intf + pde.f(x0) * tau;
         x0 = x1;
         x1 = x1 + pde.b(x1) * tau + randn(1,2) * sqrt(tau);
-    else
-        break
-    end
+    % else
+    %     break
+    % end
 end
 U = 1/2 - intf;
